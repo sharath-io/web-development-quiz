af7380fe15c99dc9ef06e91fed6261831cdef1ad
Sharath <sharath.esr@gmail.com>
2022-08-14 23:45:31 +0530

    feat: added routing for pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
+import {Routes, Route } from "react-router-dom";
+import { Home, Result, Question,Rules,Login} from "./pages";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>Web Developemnt Quiz App</h1>
-      <h2>Home Page</h2>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/question" element={<Question />} />
+      <Route path="/result" element={<Result />} />
+      <Route path="/rules" element={<Rules />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
-
-export default App;
