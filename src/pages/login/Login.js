3a13dd061bf56c692dbfc8c6d3b23a766506279c
Sharath <sharath.esr@gmail.com>
2022-08-14 23:46:06 +0530

    feat: added login page

---
+++ src/pages/login/Login.js
@@ -0,0 +1,11 @@
+import {  Navbar } from "../../components";
+export function Login(){
+    return (
+        <>
+        <Navbar/>
+        <div>
+          <h1>This is login/signup page</h1>
+        </div>
+        </>
+    )
+}
