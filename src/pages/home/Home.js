9bc9f8d22183251b89cf379484da7e743b31abf7
Sharath <sharath.esr@gmail.com>
2022-08-14 23:42:49 +0530

    feat: added home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,42 @@
+import {Navbar, Footer} from "../../components";
+import '../../styles/project.css';
+import './home.css';
+import { categories as data } from "../../backend/db/categories";
+import {Link} from "react-router-dom";
+
 export function Home(){
     return (
         <>
-        <h1>This is Home page</h1>
+        <Navbar/>
+        <section id="home-section">
+            <div className="categories" id="explore-categories">
+                Quiz categories
+            <div className="card-container">
+            {
+            data.map(data =>(
+              <div className="card"  key ={data.id}>
+                <div className="card-image fit">
+                    <img src={data.image} alt={data.categoryName} />
+                </div>
+                <div className="card-heading">
+                    {data.categoryName}
+                </div>
+                <div className="card-description">
+                    {data.description}
+                </div>
+                <div className="card-button">
+                    <Link to="/rules">
+                      <button className="card-buy">Attempt Quiz</button>
+                    </Link>
+                </div>
+               </div>
+             ))
+            }
+            </div>
+            </div>
+        
+        </section>
+        <Footer/>
         </>
     )
 }
