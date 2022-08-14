a1e3988c894cd886a752bc52f50b2dadc536ab23
Sharath <sharath.esr@gmail.com>
2022-08-14 23:46:30 +0530

    feat: added navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,17 @@
+import "./navbar.css";
+import {Link} from "react-router-dom";
+
 export function Navbar(){
-    return (
-        <>
-        <h1>This is Navbar Component</h1>
-        </>
-    )
+return (
+<nav className="navbar">
+    <div className="nav-heading">
+        <Link to="/" className="icon"> Web-Dev-Quiz</Link>
+    </div>
+    <ul className="nav-comp-list">
+        <li className="nav-inline">
+          <Link to="/login"><button  className="bt btn-primary login-button">Login/ SignUp</button></Link>
+        </li> 
+    </ul>
+</nav>
+);
 }
